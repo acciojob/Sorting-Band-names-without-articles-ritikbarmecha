82f4e83fc9160da19ad589c94529f2d745443b13
ritikbarmecha <60346351+ritikbarmecha@users.noreply.github.com>
2023-09-10 09:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
-
+//your code h
+let regex=/\bThe\b| \ban\b| \ba\b/gi;
+	let tr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+   let ans=[];
+for(let itm of  tr){
+	let x=itm.replace(regex,"").trim();
+	ans.push(x);
+}
